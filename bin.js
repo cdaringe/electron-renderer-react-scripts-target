61ac5d3b3c740efbd0458b9061cb818647153ce4
cdaringe <christopher.dieringer@gmail.com>
2018-01-03 23:41:35 -0800

    feat(bin): add bin. 1.1.0

---
+++ bin.js
@@ -0,0 +1,6 @@
+var editor = require('react-scripts-webpack-config-editor')
+var path = require('path')
+var configModifier = path.resolve(__dirname, 'index.js')
+var debug = require('debug')('electron-renderer-react-scripts-target:bin')
+debug(`configModifier: ${configModifier}`)
+editor(configModifier)
