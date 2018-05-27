78d416875441cc99ccc334ef96bbfd88d34232e9
Joshua Pinter <joshuapinter@gmail.com>
2018-05-27 11:56:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function editWebpackConfig (webpackConfig) {
-  // webpackConfig is the parsed JS webpack config from react-scrips.
-  // modify it here synchronously, & return it.
+  // webpackConfig is the parsed JS webpack config from react-scripts.
+  // modify it here synchronously & return it.
   delete webpackConfig.node
   return Object.assign({}, webpackConfig, {
     target: 'electron-renderer'
